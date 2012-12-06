a32b50bff8279ae4b4f6353f3866740902633244
sagarbommidi <sagar.b@imaginea.com>
2012-12-06 12:38:48 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OnlineStore::Application.config.secret_token = '4ea676e0ed36b0f77c7b8862291e86acd32394e75ca2b71ee9ee8a91a391460543b615baea8511045188357b72a4d31f12c998496d9e67de06aa81e48a778706'
